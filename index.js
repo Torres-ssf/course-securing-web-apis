f219dae5d7edf2fd7ace46838b0819f302fcb205
Torres-ssf <sergio.uft@gmail.com>
2020-12-18 11:52:02 -0300

    style(index file): conform index file with prettier code style

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import routes from './src/routes/crmRoutes';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import routes from "./src/routes/crmRoutes";
 
 const app = express();
 const PORT = 3000;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect("mongodb://localhost/CRMdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // bodyparser setup
@@ -20,12 +20,10 @@ app.use(bodyParser.json());
 routes(app);
 
 // serving static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+app.get("/", (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
 );
 
-app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
